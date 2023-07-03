fbf8d6c10169c8e3265dbe56c696a3b0dfc0a839
İbrahim EDİZ <edizdesign@gmail.com>
2023-07-03 07:56:47 +0000

    03072023_3

--- olustur.py
+++ olustur.py
@@ -2,9 +2,10 @@ import os
 
 # os.mkdir("Egzersiz")
 fileName = "ilk.ipynb"
-liste = ["Cevaplar"]
+liste = ["Cevaplar","Arzu","Ersel","Ozkan","Atıf","Omer","Yusuf",\
+    "Beytullah","Mehmet","İhsan","OmerReal","Beste","Şafak","Kubra","Hatice","YunusEmre","Berkay","Muhammed","Harun"]
 for item in liste:
-    folderPath = os.path.join(["Egzersiz",item])
-    if  folderPath:
+    folderPath = os.path.join("Egzersiz",item)
+    if not os.path.exists(folderPath):
         os.mkdir(folderPath)
     open(os.path.join(folderPath,fileName),"a+")
