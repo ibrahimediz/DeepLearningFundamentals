c886e4d7ee5ee7ffdc96d7f643052eb8655dd7b9
İbrahim EDİZ <edizdesign@gmail.com>
2023-07-03 08:47:04 +0000

    03072023_3

--- olustur.py
+++ olustur.py
@@ -1,7 +1,7 @@
 import os
 
 # os.mkdir("Egzersiz")
-fileName = "ilk.ipynb"
+fileName = "01_01_Pandas.ipynb"
 liste = ["Cevaplar","Arzu","Ersel","Ozkan","Atıf","Omer","Yusuf",\
     "Beytullah","Mehmet","İhsan","OmerReal","Beste","Şafak","Kubra","Hatice","YunusEmre","Berkay","Muhammed","Harun"]
 for item in liste:
